954163033eec11575bd0ab60f779a3c13e4d6116
Wes Souza <wesleydesouza@me.com>
2019-03-25 20:06:28 -0400

    Install react-use

--- web/declaration.d.ts
+++ web/declaration.d.ts
@@ -13,3 +13,7 @@ declare module 'natural-compare-lite' {
 declare module 'color-contrast' {
   export default function(a: string, b: string): number;
 }
+
+declare module 'use-callbag';
+
+declare module 'react-wait';
