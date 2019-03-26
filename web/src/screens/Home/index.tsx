ec688dccd56894bc86f430b358ae4851934ad917
Wes Souza <wesleydesouza@me.com>
2019-03-25 20:06:53 -0400

    Refactor home to use simple state

--- web/src/screens/Home/index.tsx
+++ web/src/screens/Home/index.tsx
@@ -1,11 +1,22 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
+import { useGeolocation } from 'react-use';
+import { GeoLocationSensorState } from 'react-use/lib/useGeolocation';
 
-import { ILineAdvisory } from '~/state/line';
-import { IStation } from '~/state/station';
+import {
+  lineState,
+  ILineAdvisoryByLineId,
+  lineActions,
+  ILinesById,
+} from '~/state/line';
+import { sortStationsByProximity } from '~/lib/sortStationsByProximity';
+import {
+  IStation,
+  stationState,
+  IStationPlatformsByStationId,
+  stationActions,
+} from '~/state/station';
 
-import LineAdvisories from '~/components/LineAdvisories';
-import Query, { IQueryResult } from '~/components/Query';
 import TimeTable from '~/components/TimeTable';
 
 interface IProps {
@@ -14,29 +25,124 @@ interface IProps {
 
 import styles from './styles.css';
 
-class Home extends React.Component<IProps> {
-  public render() {
-    return (
+const Home = (_props: IProps) => {
+  // FIXME: remove type forcing if https://github.com/streamich/react-use/pull/171 is released
+  const {
+    latitude,
+    longitude,
+  } = (useGeolocation() as any) as GeoLocationSensorState;
+
+  const [linesById] = lineState.useObserver<ILinesById | null>(
+    ({ linesById }) => linesById,
+  );
+
+  const [stations] = stationState.useObserver<IStation[] | null>(
+    ({ stationsById }) => (stationsById ? Object.values(stationsById) : null),
+  );
+
+  const [
+    platformsByStationId,
+  ] = stationState.useObserver<IStationPlatformsByStationId | null>(
+    ({ platformsByStationId }) => platformsByStationId,
+  );
+
+  const [
+    lineAdvisoriesByLineId,
+  ] = lineState.useObserver<ILineAdvisoryByLineId | null>(
+    ({ lineAdvisoriesByLineId }) => lineAdvisoriesByLineId,
+  );
+
+  const [sortedStations, setSortedStations] = useState<IStation[]>([]);
+  const [sortedStationIds, setSortedStationIds] = useState<string[]>([]);
+  const [lineIds, setLineIds] = useState<string[]>([]);
+
+  const fetchAdvisories = (lineIds: string[]) => {
+    lineIds.forEach(lineId => {
+      lineActions.fetchLineAdvisories(lineId);
+    });
+  };
+
+  const fetchStationPlatforms = (stationIds: string[]) => {
+    stationIds.forEach(stationId => {
+      stationActions.fetchStationPlatformsByStationId(stationId);
+    });
+  };
+
+  const reload = () => {
+    fetchStationPlatforms(sortedStationIds);
+    fetchAdvisories(lineIds);
+  };
+
+  useEffect(() => {
+    if (!stations || !latitude || !longitude) {
+      return;
+    }
+
+    const localSortedStations = sortStationsByProximity(
+      stations,
+      latitude,
+      longitude,
+    ).slice(0, 5);
+    setSortedStations(localSortedStations);
+    setSortedStationIds(localSortedStations.map(station => station.id));
+
+    setLineIds([
+      ...new Set(localSortedStations.map(station => station.lineIds).flat()),
+    ]);
+  }, [stations, latitude, longitude]);
+
+  useEffect(() => {
+    fetchStationPlatforms(sortedStationIds);
+    fetchAdvisories(lineIds);
+  }, [sortedStationIds.join(), lineIds.join()]);
+
+  if (
+    !stations ||
+    !latitude ||
+    !longitude ||
+    !lineAdvisoriesByLineId ||
+    !platformsByStationId ||
+    !linesById
+  ) {
+    return <div>Loading or error.</div>;
+  }
+
+  return (
+    <>
+      <Helmet>
+        <title>SubwayTi.me</title>
+      </Helmet>
       <div className={styles.Home}>
-        <Helmet>
-          <title>Subway Ti.me</title>
-          <link rel="shortcut icon" href="/icons/S.png" type="image/png" />
-          <link rel="apple-touch-icon" href="/icons/S@8x.png" />
-        </Helmet>
-        <Query
-          query={getNearbyStations(4)}
-          renderWhenError={this.renderNoLocation}
-          renderWhenLoading={
-            <React.Fragment>
-              <TimeTable.Skeleton />
-              <TimeTable.Skeleton />
-            </React.Fragment>
-          }
-        >
-          {({ data }: IQueryResult<string[]>) =>
-            data.map(stationId => this.renderTimeTable(stationId))
-          }
-        </Query>
+        {sortedStations
+          .filter(station => platformsByStationId[station.id])
+          .map(station => (
+            <TimeTable
+              advisories={station.lineIds
+                .map(lineId => lineAdvisoriesByLineId[lineId])
+                .filter(Boolean)
+                .flat()}
+              key={station.id}
+              lastUpdate={0}
+              linesById={linesById}
+              station={station}
+              platforms={platformsByStationId[station.id]}
+              updateData={reload}
+            />
+          ))}
+        {/* <Query
+        query={getNearbyStations(4)}
+        renderWhenError={renderNoLocation}
+        renderWhenLoading={
+          <React.Fragment>
+            <TimeTable.Skeleton />
+            <TimeTable.Skeleton />
+          </React.Fragment>
+        }
+      >
+        {({ data }: IQueryResult<string[]>) =>
+          data.map(stationId => renderTimeTable(stationId))
+        }
+      </Query> */}
         <div className={styles.credits}>
           <p className={styles.smaller}>
             Built with <span className={styles.love}>&lt;3</span> by{' '}
@@ -47,71 +153,13 @@ class Home extends React.Component<IProps> {
           </p>
           <p className={styles.smaller}>
             <a href="https://github.com/WesSouza/subway-time" target="_blank">
-              Fork it on GitHub.
+              Source code available on GitHub.
             </a>
           </p>
         </div>
       </div>
-    );
-  }
-
-  public renderTimeTable(stationId: string) {
-    return (
-      <Query
-        key={stationId}
-        query={getStationWithTimes}
-        parameters={{ stationId }}
-        renderWhenError={
-          <div className={styles.centralized}>
-            Unable to retrieve time table.
-          </div>
-        }
-        renderWhenLoading={<TimeTable.Skeleton />}
-      >
-        {({ data, lastUpdate, updateData }: IQueryResult<IStation>) => (
-          <TimeTable
-            lastUpdate={lastUpdate}
-            station={data}
-            advisoriesComponent={
-              <Query
-                query={getAdvisoriesForLines}
-                parameters={{ lineIds: data.lineIds }}
-              >
-                {({ data: advisoryData }: IQueryResult<ILineAdvisory[]>) => (
-                  <LineAdvisories advisories={advisoryData} />
-                )}
-              </Query>
-            }
-            updateData={updateData}
-          />
-        )}
-      </Query>
-    );
-  }
-
-  public renderNoLocation = () => {
-    return (
-      <div className={styles.error}>
-        <div className={styles.errorMessage}>
-          Unable to find nearby stations.
-          <br />
-          <br />
-          Please allow location access.
-        </div>
-        <button
-          className={styles.errorRetry}
-          onClick={this.handleRetryClick}
-          type="button"
-        >
-          Try again
-        </button>
-      </div>
-    );
-  };
-
-  public handleRetryClick = () => {
-    location.reload();
-  };
-}
+    </>
+  );
+};
 
 export default Home;
